79a176c8b4a960a418d6b0edfe99e350abecdfb9
sepalaznik <se.palaznik@yandex.by>
2021-10-21 22:17:33 +0200

    BrowserRouter => HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "macro-css";
 
 import "./index.scss";
@@ -8,9 +8,9 @@ import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
